704d3c24bce34f547e9186e0472711681dcccb43
Fredrik Malmo <fredrik@kodeworks.no>
2019-11-29 15:08:28 +0100

    Don't crash if no auth token

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,11 @@ import App from "./components/App";
 import { fetchToken, loadToken } from "./artillery/API";
 
 if (!loadToken()) {
-  fetchToken();
+  fetchToken().catch(() =>
+    console.warn(
+      "It appears you have not provided the token required for the app to run."
+    )
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
